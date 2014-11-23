4377bcc43ea6ba837a162abfe27756b18cdfabb3
briKou <briKou@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 15:03:18 +0000

    = 1.6.1.1 = * Logo on plugin directory list * Remove unnecessary files from trunk
    git-svn-id: https://plugins.svn.wordpress.org/wp-plugin-info-card/trunk@1031175 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-plugin-info-card.php
+++ wp-plugin-info-card.php
@@ -5,7 +5,7 @@
  * Description: WP Plugin Info Card displays plugins identity cards in a beautiful box with a smooth rotation effect using WP Plugin API. Dashboard widget included.
  * Author: Brice CAPOBIANCO
  * Author URI: http://b-website.com/
- * Version: 1.6.1
+ * Version: 1.6.1.1
  * Text Domain: wppic-translate
  */
 
@@ -85,7 +85,7 @@ add_filter("plugin_action_links_".WPPIC_BASE, 'wppic_settings_link' );
 if ( ! function_exists( 'wppic_meta_links' ) ) {
 	function wppic_meta_links( $links, $file ) {
 		if ( strpos( $file, 'wp-plugin-info-card.php' ) !== false ) {
-			$links[0] = '<a href="http://b-website.com/" target="_blank"><img src="' . AN_URL . 'img/icon-bweb.png" style="margin-bottom: -4px;" alt="b*web"/></a>&nbsp;&nbsp;'. $links[0];
+			$links[0] = '<a href="http://b-website.com/" target="_blank"><img src="' . WPPIC_URL . 'img/icon-bweb.png" style="margin-bottom: -4px;" alt="b*web"/></a>&nbsp;&nbsp;'. $links[0];
 			$links = array_merge( $links, array( '<a href="http://b-website.com/wp-plugin-info-card-for-wordpress" target="_blank" title="'. __( 'Documentation and examples', 'an-translate' ) .'"><strong style="color:#db3939">'. __( 'Documentation and examples', 'an-translate' ) .'</strong></a>' ) );
 			$links = array_merge( $links, array( '<a href="http://b-website.com/category/plugins" target="_blank" title="'. __( 'More b*web Plugins', 'an-translate' ) .'">'. __( 'More b*web Plugins', 'an-translate' ) .'</a>' ) );
 			$links = array_merge( $links, array( '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7Z6YVM63739Y8" target="_blank" title="'. __( 'Donate', 'an-translate' ) .'"><strong>'. __( 'Donate', 'an-translate' ) .'</strong></a>' ) );
